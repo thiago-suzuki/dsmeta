932ef22a07d1772f143f923a84ee3d5b4d085e84
Thiago Suzuki <thiagoikenaga@gmail.com>
2022-07-13 23:55:35 -0300

    Test Send SMS Heroku

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmeta.services;
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,12 +32,14 @@ public class SmsService {
 	
 	public void sendSms(Long saleId) {
 
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(2);
 		Sale sale = saleRepository.findById(saleId).get();
 		
 		String date = sale.getDate().getMonthValue() + "/" +  sale.getDate().getYear();
 		
 		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date + 
-				" com um total de R$ " + String.format("%.0f", sale.getAmount());
+				" com um total de R$ " + df.format(sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
